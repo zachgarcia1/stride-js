f243ff8cc3f447ec3e6cdcfc5b3ff72eac692b72
Zach Garcia <zach.garcia@lanterralever.com>
2016-11-13 09:31:14 -0700

    update module options vars

--- js/stride.js
+++ js/stride.js
@@ -22,17 +22,17 @@ $.fn.stride = function (options) {
 
             onLoad: function () {
                 var elementOffset = this.$el.offset().top;
-                var backgroundX = this.options.backgroundX;
-                var backgroundY = this.options.backgroundY;
-                var backgroundAcceleration = this.options.backgroundAcceleration;
+                var backgroundX = this.options.x;
+                var backgroundY = this.options.y;
+                var backgroundAcceleration = this.options.acceleration;
 
                 this.$el.css('background-position', backgroundX + '% ' + (backgroundY + Math.max(document.body.scrollTop - elementOffset) / (backgroundAcceleration * .5)) + '%')
             },
 
             onScroll: function () {
-                var backgroundX = this.options.backgroundX;
-                var backgroundY = this.options.backgroundY;
-                var backgroundAcceleration = this.options.backgroundAcceleration;
+                var backgroundX = this.options.x;
+                var backgroundY = this.options.y;
+                var backgroundAcceleration = this.options.acceleration;
 
                 if (this.$el.length) {
                     var height = this.$el.outerHeight();
@@ -59,7 +59,7 @@ $(function(){
     $('[data-stride]').stride({
         x: 50,
         y: 50,
-        a:-20
+        acceleration:-20
     });
 
 });
